2e431bc2ca7ac7e7b29c22882e3b126eff0882f3
Martin Delebecque <martin-delebecque@outlook.fr>
2024-04-16 11:55:42 +0200

    Add a btn and a form to the logbook page in order to add a new log

--- src/features/logbook/Logbook.jsx
+++ src/features/logbook/Logbook.jsx
@@ -1,5 +1,72 @@
+import { useState } from 'react';
+
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 export default function Logbook() {
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <h1>Logbook</h1>
+        <div className="signals">
+            <h1>Logbook</h1>
+
+            {/* Add button */}
+            <Fab
+                color="primary"
+                aria-label="add"
+                sx={{ position: 'fixed', right: 20, bottom: 80 }}
+                onClick={handleClickOpen}>
+                <AddIcon />
+            </Fab>
+
+            {/* Add modal */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        console.log(formJson);
+                        handleClose();
+                    }
+                }}>
+                <DialogTitle>New log</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="object"
+                        name="object"
+                        label="Object"
+                        type="text"
+                        fullWidth
+                        variant="outlined" />
+                    <TextField
+                        required
+                        id="outlined-multiline-static"
+                        label="Content"
+                        multiline
+                        rows={10} sx={{ marginTop: 1 }}
+                        fullWidth={true} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button type="submit">Subscribe</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 } 
