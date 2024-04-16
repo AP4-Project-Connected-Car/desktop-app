3a2e8eb46d869973659811b69424e2542c6bc7b3
Martin Delebecque <martin-delebecque@outlook.fr>
2024-04-16 11:09:43 +0200

    Add an error message when data are not reachable (signals)

--- src/features/signals/SignalCard.jsx
+++ src/features/signals/SignalCard.jsx
@@ -1,8 +1,47 @@
-export default function SignalCard({ name, imgName }) {
+import { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: 'none',
+    boxShadow: 24,
+    p: 4,
+    paddingBottom: 0
+  };
+
+export default function SignalCard({ name, description, imgName }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <div className="data-card">
-            <h3>{ name }</h3>
-            <img src={ imgName } alt={name} />
-        </div>
+        <>
+            <div className="data-card" onClick={handleOpen}>
+                <h3>{ name }</h3>
+                <img src={ imgName } alt={name} />
+            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        { name }
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }} dangerouslySetInnerHTML={{__html: description}}>
+                    </Typography>
+                    <img src={ imgName } alt={name} style={{ display: 'block', margin: 'auto' }} />
+                </Box>
+            </Modal>
+        </>
     );
 }
--- src/features/signals/Signals.jsx
+++ src/features/signals/Signals.jsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react';
+
+import Alert from '@mui/material/Alert';
+
 import SignalCard from './SignalCard';
 
 export default function Signals() {
     const [signalsList, setSignalsList] = useState([]);
+    const [isFailed, setIsFailed] = useState(false);
 
 	useEffect(() => {
 		fetch('http://localhost:8080/signals')
             .then(response => response.json())
-            .then(data => setSignalsList(data));
+            .then(data => setSignalsList(data))
+            .catch(err => {
+                console.error(err);
+                setIsFailed(true);
+            });
 	}, []);
 
     return (
         <div className="signals">
             <h1>Signals</h1>
             <main>
-                { signalsList.map(signal => (
+                { !isFailed ? signalsList.map(signal => (
 					<SignalCard {...signal} key={signal.id} />
-				)) }
+				)) : 
+                    <div className="error-div">
+                        <Alert variant="filled" severity="error">An error has occured when trying accessing data.</Alert>
+                    </div>
+                }
             </main>
         </div>
     );
